af1f69bf120502e2e1c3b88aed98c3d0d0dcd1f7
박은성 <clans2021@naver.com>
2022-02-15 01:16:54 +0900

    작업커밋

---
+++ LabelEditor/TestDataForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelEditor
+{
+    public partial class TestDataForm : Form
+    {
+        public TestDataForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_0_Send_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_2_IP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
